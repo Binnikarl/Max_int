46324fb2ed076678c5108ac42a392e60b03b2a73
Brynjar Karl Ævarsson <Brynjarka19@ru.is>
2020-09-03 14:17:10 +0000

    Finished writing algorithm

--- sequence.py
+++ sequence.py
@@ -1,1 +1,12 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+n1 = 0
+n2 = 1
+n3 = 0
+
+for i in range(n):
+    nsum = n1 + n2 + n3
+    n1 = n2
+    n2 = n3
+    n3 = nsum
+    print(nsum)
